6a42c13f5b908b1b49d0fb783824faedd255c5df
jubernar <jubernar@pcgrisbernard.ad.igd.fraunhofer.de>
2017-08-22 23:17:11 +0200

    Canopy

--- src/main/java/com/github/TKnudsen/DMandML/model/unsupervised/clustering/impl/Cobweb.java
+++ src/main/java/com/github/TKnudsen/DMandML/model/unsupervised/clustering/impl/Cobweb.java
@@ -11,7 +11,14 @@ import main.java.com.github.TKnudsen.DMandML.model.unsupervised.clustering.WekaC
  * </p>
  * 
  * <p>
- * Description: implementation is based on WEKAs Cobweb.
+ * Description: incremental system for hierarchical conceptual clustering.
+ * Implementation is based on WEKAs Cobweb.
+ * 
+ * D. Fisher (1987). Knowledge acquisition via incremental conceptual
+ * clustering. Machine Learning. 2(2):139-172.
+ * 
+ * J. H. Gennari, P. Langley, D. Fisher (1990). Models of incremental concept
+ * formation. Artificial Intelligence. 40:11-61.
  * </p>
  * 
  * <p>
@@ -27,13 +34,22 @@ public class Cobweb extends WekaClusteringAlgorithm {
 
 	private double cutoff;
 
+	private int seed;
+
 	protected Cobweb() {
-		this(8, 12);
+		this(1.0, 0.002);
 	}
 
 	public Cobweb(double acuity, double cutoff) {
 		setAcuity(acuity);
 		setCutoff(cutoff);
+		setSeed(17);
+	}
+
+	public Cobweb(double acuity, double cutoff, int seed) {
+		setAcuity(acuity);
+		setCutoff(cutoff);
+		setSeed(seed);
 	}
 
 	@Override
@@ -52,19 +68,39 @@ public class Cobweb extends WekaClusteringAlgorithm {
 	protected void initializeClusteringAlgorithm() {
 		// Valid options are:
 		//
-		// -A <0-100>
+		// -A <acuity>
 		// Acuity.
+		// (default=1.0) 0-100
 		//
-		// -C <0-100>
+		// -C <cutoff>
 		// Cutoff.
+		// (default=0.002) 0-100
+		//
+		// -save-data
+		// Save instance data.
+		//
+		// -S <num>
+		// Random number seed.
+		// (default 42)
+		//
+		// -output-debug-info
+		// If set, clusterer is run in debug mode and
+		// may output additional info to the console
+		//
+		// -do-not-check-capabilities
+		// If set, clusterer capabilities are not checked before clusterer is
+		// built
+		// (use with caution).
 
 		wekaClusterer = new weka.clusterers.Cobweb();
 
-		String[] opts = new String[4];
+		String[] opts = new String[6];
 		opts[0] = "-A";
 		opts[1] = "" + getAcuity();
 		opts[2] = "-C";
 		opts[3] = "" + getCutoff();
+		opts[4] = "-S";
+		opts[5] = "" + getSeed();
 
 		try {
 			wekaClusterer.setOptions(opts);
@@ -87,6 +123,14 @@ public class Cobweb extends WekaClusteringAlgorithm {
 
 	public void setCutoff(double cutoff) {
 		this.cutoff = cutoff;
+	}
+
+	public int getSeed() {
+		return seed;
+	}
+
+	public void setSeed(int seed) {
+		this.seed = seed;
 	}
 
 }
