2051ec35ba4229908eb3243952030eabe7fd780f
ChristianRitter <christian.ritter.alz@web.de>
2017-08-31 17:08:02 +0200

    PairwiseDistanceMatrix now directly implements IDistanceMatrix instead of extending DistanceMatrix due to having no distance measure

--- src/main/java/com/github/TKnudsen/DMandML/data/distanceMatrix/PairwiseDistanceMatrix.java
+++ src/main/java/com/github/TKnudsen/DMandML/data/distanceMatrix/PairwiseDistanceMatrix.java
@@ -1,8 +1,10 @@
 package main.java.com.github.TKnudsen.DMandML.data.distanceMatrix;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import com.github.TKnudsen.ComplexDataObject.data.distanceMatrix.DistanceMatrix;
+import com.github.TKnudsen.ComplexDataObject.data.distanceMatrix.IDistanceMatrix;
 
 /**
  * <p>
@@ -13,19 +15,86 @@ import com.github.TKnudsen.ComplexDataObject.data.distanceMatrix.DistanceMatrix;
  * Description:
  * </p>
  * 
- * <p>
- * Copyright: (c) 2016-2017 Juergen Bernard
- * </p>
- * 
  * @author Christian Ritter
- * @version 1.01
+ * @version 1.02
  */
-public class PairwiseDistanceMatrix<T> extends DistanceMatrix<T> {
+public class PairwiseDistanceMatrix<T> implements IDistanceMatrix<T> {
+
+	private double[][] distanceMatrix;
+	private Map<T, Integer> objectMapping;
+	private List<T> elements;
+	private double minDistance;
+	private double maxDistance;
 
 	public PairwiseDistanceMatrix(List<T> objects, double[][] pairwiseDistances) {
-		super(objects, null);
 		this.distanceMatrix = pairwiseDistances;
-		initializeObjectIndex();
+		this.elements = objects;
+		initializeObjectMapping();
+		calculateMinMaxDistance();
+	}
+
+	private void calculateMinMaxDistance() {
+		minDistance = Double.MAX_VALUE;
+		maxDistance = Double.MIN_VALUE;
+		for (int i = 0; i < distanceMatrix.length; i++) {
+			for (int j = i; j < distanceMatrix.length; j++) {
+				minDistance = Math.min(minDistance, distanceMatrix[i][j]);
+				maxDistance = Math.max(maxDistance, distanceMatrix[i][j]);
+			}
+		}
+	}
+
+	private void initializeObjectMapping() {
+		objectMapping = new HashMap<>();
+		int i = 0;
+		for (T t : getElements()) {
+			objectMapping.put(t, i++);
+		}
+	}
+
+	@Override
+	public double getDistance(T o1, T o2) {
+		Integer i = objectMapping.get(o1);
+		Integer j = objectMapping.get(o2);
+		if (i == null || j == null) {
+			return Double.NaN;
+		}
+		return getDistanceMatrix()[i][j];
+	}
+
+	@Override
+	public double applyAsDouble(T t, T u) {
+		return getDistance(t, u);
+	}
+
+	@Override
+	public String getName() {
+		return "PairwiseDistanceMatrix";
+	}
+
+	@Override
+	public String getDescription() {
+		return "IDistanceMatrix Wrapper for pairwise distances represented as 2D double array";
+	}
+
+	@Override
+	public double[][] getDistanceMatrix() {
+		return distanceMatrix;
+	}
+
+	@Override
+	public List<T> getElements() {
+		return elements;
+	}
+
+	@Override
+	public double getMinDistance() {
+		return minDistance;
+	}
+
+	@Override
+	public double getMaxDistance() {
+		return maxDistance;
 	}
 
 }
