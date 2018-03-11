5486fd5757eb86840caa1e151c139723e952cfa5
jubernar <jubernar@pcgrisbernard.ad.igd.fraunhofer.de>
2018-03-11 13:51:49 +0100

    linkage enum

---
+++ src/main/java/com/github/TKnudsen/DMandML/model/unsupervised/clustering/enums/LinkageStrategy.java
@@ -0,0 +1,22 @@
+package com.github.TKnudsen.DMandML.model.unsupervised.clustering.enums;
+
+/**
+ * <p>
+ * Title: LinkageStrategy
+ * </p>
+ * 
+ * <p>
+ * Description:
+ * </p>
+ * 
+ * <p>
+ * Copyright: (c) 2016-2018 Juergen Bernard, https://github.com/TKnudsen/DMandML
+ * </p>
+ * 
+ * @author Juergen Bernard
+ * @version 1.01
+ */
+
+public enum LinkageStrategy {
+	SingleLinkage, AverageLinkage, MedianLinkage, CompleteLinkage;
+}
