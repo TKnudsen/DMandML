dbe7cac2818b33160140edf6c44adb39ff88d86e
Markus Lehmann <markus.lehmann@ymail.com>
2019-04-29 18:54:12 +0200

    Name fix

--- src/main/java/com/github/TKnudsen/DMandML/model/degreeOfInterest/model/classifiers/spatialization/classRelations/characteristics/ClassBordersInterestingnessFunction.java
+++ src/main/java/com/github/TKnudsen/DMandML/model/degreeOfInterest/model/classifiers/spatialization/classRelations/characteristics/ClassBordersInterestingnessFunction.java
@@ -77,7 +77,7 @@ public class ClassBordersInterestingnessFunction<FV>
 
     @Override
     public String getName() {
-        return "Class Borders";
+        return "Class Borders  [" + getClassifierName() + "]";
     }
 
     @Override
